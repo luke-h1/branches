49d00d81794fb74caa59bf9e2443bb2ea6c6025b
luke-h1 <luke.howsam@yahoo.com>
2024-09-18 10:04:48 +0100

    feat(frontend): add link to uptime dashboard

--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -1,4 +1,6 @@
+import { FiExternalLink } from "react-icons/fi";
 import styles from "./PageHeader.module.scss";
+import Link from "../Link/Link";
 
 interface Props {
   title: string | JSX.Element;
@@ -12,6 +14,11 @@ const PageHeader = ({ title, children, compact, description }: Props) => {
     <div className={compact ? styles.wrapperCompact : styles.wrapper}>
       <h1 className={styles.title}>{title}</h1>
       {description && <p className={styles.description}>{description}</p>}
+      <div className={styles.uptime}>
+        <Link href="https://status.lhowsam.com">Uptime dashboard</Link>{" "}
+        <FiExternalLink className={styles.extLink} />
+      </div>
+
       {children}
     </div>
   );
